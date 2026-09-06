a938e9e42dbab0d91e7e81a10d6d09c256a1d376
Sachid Tiwari <sachidtiwari333@gmail.com>
2026-09-06 22:27:51 +0545

    some changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/web/hero";
-import Image from "next/image";
+import { Services } from "@/components/web/services";
+
 
 export default function Home() {
   return (
     <>
     <Hero></Hero>
+    <Services></Services>
     </>
   );
 }
---
+++ client/components/web/services.tsx
@@ -0,0 +1,16 @@
+import { Card } from "../ui/card";
+
+export function Services () {
+  return (
+    <section>
+      <div>
+        <h1>Our Services</h1>
+      </div>
+      <div>
+        <div>
+          <Card />
+        </div>
+      </div>
+    </section>
+  )
+}
